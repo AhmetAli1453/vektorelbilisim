7b4abbb1a24fae6af8884765b242f68c28f7f0f4
AhmetAli1453 <benliahmetali41@gmail.com>
2025-07-12 11:10:54 +0300

    değişiklikler

--- 2.py
+++
@@ -1,4 +0,0 @@
-integer = 5 , 10 , 15 # sayılardır
-string = "5, 10, 15" # stringdir yazılardır
-float = 5.0 , 10.0 , 15.0 # float sayılardır küsüreatlı virgüllü sayılardır
-boolean = True , False # boolean değerleridir doğru ve yanlış değerleridir teşekkürler :)
